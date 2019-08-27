1a7e282f859bdf9ea7d83d857b369dd747e40abf
Shriya Bansal <bansals@uchicago.edu>
2019-08-27 17:46:59 -0500

    updated typing and scrolling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   window.onscroll = function() {myFunction()};
+//
+//   var navbar = document.getElementById("main-nav");
+//   var sticky = navbar.offsetTop;
+//
+//   function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//       navbar.classList.add("sticky")
+//     } else {
+//       navbar.classList.remove("sticky");
+//     }
+//   }
+// })
+
+var withBreaks = "Hello World. \n My name is Jennifer. \n What is your name?"
+
+
+$(document).ready(function() {
+  $('#main-nav li a').click(function(e) {
+
+    var targetHref = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(targetHref).offset().top
+    }, 1000);
+
+    e.preventDefault();
+  });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-  window.onscroll = function() {myFunction()};
+  var i = 0;
 
-  var header = document.getElementById("logo");
-  var sticky = header.offsetTop;
+  var txt = "software developer, sophomore at UChicago, and advocate for women in tech";
+  var speed = 50;
 
-  function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
   }
+
+  // typeWriter()
 })
